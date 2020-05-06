1b149161fc753a733c7ad0e278da09bd0253469b
Aaron Schuyler <asky28@gmail.com>
2020-05-06 19:58:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(calls)
   calls.any? { |call| call.length > 4}
 end
 
-def find_the_cheese(snacks)
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0
   while i < cheese_types.length
